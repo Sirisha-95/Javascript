e5c16f6433b247a34b09a89e2cbc30e779aeed2b
Sirisha-95 <82671383+Sirisha-95@users.noreply.github.com>
2023-09-07 12:35:13 +0200

    Create Objects.js

---
+++ Objects.js
@@ -0,0 +1,12 @@
+/* Using dot and bracket operator to access object properties in javascript */
+
+let PersonObj={firstName:'Sirisha',
+               lastName:'D',
+               age:27,
+               hobbies:['dancing','listening to music'] }
+
+console.log(PersonObj.age); //to directly access the property
+
+console.log(PersonObj.hobbies[0]) // to compute the object key 
+
+
