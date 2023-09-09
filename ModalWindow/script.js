d2819dbdef956f7e6cf408a3253c8d6230d2fedc
Sirisha-95 <82671383+Sirisha-95@users.noreply.github.com>
2023-09-09 14:47:01 +0200

    Add files via upload

---
+++ ModalWindow/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const btnmodalList = document.querySelectorAll('.show-modal');
+console.log(btnmodalList);
+const modal = document.querySelector('.modal');
+const closemodal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+console.log(modal);
+
+const openModalFn = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnmodalList.length; i++) {
+  btnmodalList[i].addEventListener('click', openModalFn);
+}
+
+const closeModalFn = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+closemodal.addEventListener('click', closeModalFn);
