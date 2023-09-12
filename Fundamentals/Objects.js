694b724fbf6e80f0ef8d5c39dcbe946ca80d2d1b
Sirisha-95 <82671383+Sirisha-95@users.noreply.github.com>
2023-09-12 15:57:52 +0200

    Update Objects.js
    Enhanced object literals feature in ES6

--- Fundamentals/Objects.js
+++ Fundamentals/Objects.js
@@ -10,3 +10,17 @@ console.log(PersonObj.age); //to directly access the property
 console.log(PersonObj.hobbies[0]) // to compute the object key 
 
 
+//Enhanced object literals
+const Months = ['jan', 'feb', 'march'];
+const PresentDate = {
+    currentMonth: 'September',
+    Months, // 1. directly use the same object name without a property name
+    Date: '12.09.2023',
+    presentMonth() { //2. directly write function property without using function keyword
+        return 'September';
+    },
+    [Months[0]]: 'First month' // 3. compute the property name
+};
+
+console.log(PresentDate); //o/p - {currentMonth: 'September', Months: Array(3), Date: '12.09.2023', jan: 'First month'}
+console.log(PresentDate.presentMonth());//o/p- September
