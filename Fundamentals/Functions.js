e8296e96d8fc4881a1176960542ae7f040d77805
Sirisha-95 <82671383+Sirisha-95@users.noreply.github.com>
2023-09-14 15:32:51 +0200

    Update Functions.js
    high order level functions

--- Fundamentals/Functions.js
+++ Fundamentals/Functions.js
@@ -14,3 +14,31 @@ const calAge1= function(year){
 }
 
 const myAge1= calAge1(2023); // the constant calAge1 to be defined before in order to return the value
+
+//high order functions - that offers high level of abstraction 
+
+const lowFn1 = function (str) {
+    return `//g ${str}`;
+}
+
+const lowFn2 = function (str) {
+    return str.replace('o', '*');
+}
+
+//higher order function
+const highOrderFn = function (str, fn) {
+    return console.log(fn(str));
+}
+
+highOrderFn('Welcome to fn1', lowFn1);
+highOrderFn('welcome to fn2', lowFn2);
+
+//JS vital concept - callback functions that offers high and low levels of abstraction
+const clickedAction = function () {
+    return console.log('clicked!');
+}
+
+document.body.addEventListener('click', clickedAction);
+
+// callback function used in arrays
+['element1', 'element2', 'element3'].forEach(clickedAction);
