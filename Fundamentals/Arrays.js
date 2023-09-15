94fe34bc805fb2e4ae365471f15d9dcbbb34208d
Sirisha-95 <82671383+Sirisha-95@users.noreply.github.com>
2023-09-15 15:59:39 +0200

    Update Arrays.js
    array methods

--- Fundamentals/Arrays.js
+++ Fundamentals/Arrays.js
@@ -23,3 +23,33 @@ for(let i=0; i<bills.length; i++){
 }
 console.log(tips);
 console.log(totals);
+
+
+//array methods
+
+//slice method
+const arr1 = ['x', 'y', 'z', 'a', 'b'];
+
+console.log(arr1.slice(2, 4)); //includes the index 2 and excludes index 4 (4-2 =2 elements - o/p - z,a
+console.log(arr1.slice(-1)); //o/p- b
+console.log(arr1.slice(1, -2));// starts at index 1 and excludes last 2 elements - o/p- y,z
+
+//splice method
+const arr2 = ['app', 'ball', 'cat'];
+
+console.log(arr2.splice(1)); //o/p- ['bat','cat'] //mutates the array
+console.log(arr2); //o/p- ['app']
+
+//reverse
+console.log(arr2.reverse()); //mutates the array
+
+//concat -doesnt mutate arrays
+console.log(arr1.concat(arr2));
+
+//at method - also works for strings
+console.log(arr1.at(-1)); //get last element of array
+
+//Loop over arrays using 'of' or 'forEach' method 
+arr1.forEach(function (elem, index, array) {
+  console.log(elem, index, array);
+});
